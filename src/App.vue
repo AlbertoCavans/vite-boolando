439269fb86a1ebdaf423fe36f62170b54efd90c5
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-13 15:20:14 +0100

    Feat App.vue - basic structure

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Boolando",
+    };
+  },
+};
+</script>
+
+<template>
+  <header></header>
+
+  <main></main>
+
+  <footer></footer>
+</template>
+
+<style></style>
