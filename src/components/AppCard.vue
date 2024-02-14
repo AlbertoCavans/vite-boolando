32fe30963ca374a4e738af5385d73366acdbc423
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-15 00:31:40 +0100

    Feat AppCard.vue - making component AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: {
+    img: String,
+    brand: String,
+    productName: String,
+    productPrice: Number,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="img" :alt="productName" />
+
+    <div class="info-product">
+      <h6 class="product-brand text-secondary">{{ brand }}</h6>
+      <h5 class="product-name">{{ productName }}</h5>
+      <h6>
+        {{ productPrice }} <span>{{ productPrice }}</span>
+      </h6>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  width: calc(100% / 3);
+  padding: 10px;
+}
+</style>
