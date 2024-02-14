b71eabaf6d6a731fc7fe75b6fdece9b6091dedb5
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-14 12:25:43 +0100

    Feat AppHeader - main blocs & array

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,74 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        {
+          text: "Uomo",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Donna",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Bambino",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <header>
-    <h2>My App</h2>
+    <div class="container">
+      <nav class="menu">
+        <ul>
+          <li v-for="link in links">
+            <a :href="link.url">
+              {{ link.text }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="logo">
+        <a href="#">
+          <img src="../assets/img/boolean-logo.png" alt="" />
+        </a>
+      </div>
+
+      <div class="actions">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
   </header>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use "../styles/partials/mixins" as *;
+
+header {
+  @include center-content();
+  background-color: orange;
+  color: white;
+  padding: 10px;
+
+  ul li {
+    display: inline-block;
+    padding: 0 5px;
+  }
+
+  .logo a img {
+    width: 200px;
+  }
+}
+</style>
