96a9c3a7fadeba0897c93a1f11cbb76de91c2ee4
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-14 17:48:12 +0100

    Feat main.js - import font-awesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,18 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import * as bootstrap from "bootstrap";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faPhone);
+library.add(faUser);
+library.add(faHeart);
+library.add(faShoppingBag);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
