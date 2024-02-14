bdfce294eebe100d82c7159082f1b6da546e3403
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-14 13:43:17 +0100

    Feat footer - basic structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,8 +4,33 @@ export default {};
 
 <template>
   <footer>
-    <h2>My App</h2>
+    <div class="container">
+      <div class="legal-section">
+        <ul>
+          Boolando s.r.l.
+          <li>Informazioni legali</li>
+          <li>Informazioni sulla privacy</li>
+          <li>Diritto di recesso</li>
+        </ul>
+      </div>
+      <div class="socials">
+        <ul>
+          Trovaci anche su
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
   </footer>
 </template>
 
-<style></style>
+<style scoped>
+footer {
+  background-color: #1a1a1a;
+  padding: 20px 0;
+  color: white;
+}
+</style>
