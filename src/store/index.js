89492f895aca85539c59cf7d307fa9396b36d1ab
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-16 07:39:01 +0100

    Feat index.js - alert-data

--- src/store/index.js
+++ src/store/index.js
@@ -5,4 +5,11 @@ export const urlAPI = "http://localhost:3000";
 export const store = reactive({
   test: "Slogan",
   urlAPI: "http://localhost:3000",
+
+  alert: {
+    show: false,
+    message: "",
+    type: "",
+    icon: "fa-rectangle-xmark",
+  },
 });
