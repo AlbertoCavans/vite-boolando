940eaf9f437a7d9bdd312b4866c3d99474928f60
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-16 07:37:26 +0100

    Feat AppAlert.vue - basic structure

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,53 @@
+<script>
+import { store } from "../store/index";
+
+export default {
+  data() {
+    return { store };
+  },
+
+  methods: {
+    cancelAlert() {
+      store.alert.show = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="alert" :class="store.alert.type">
+    <span>{{ store.alert.message }}</span>
+
+    <font-awesome-icon
+      class="mx-2 toClick"
+      @click="cancelAlert()"
+      :icon="'fa-solid ' + store.alert.icon"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.toClick {
+  cursor: pointer;
+}
+.alert {
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+}
+.alert.danger {
+  background-color: red;
+}
+
+.alert.warning {
+  background-color: yellow;
+}
+
+.alert.success {
+  background-color: green;
+}
+.alert.info {
+  background-color: cyan;
+}
+</style>
