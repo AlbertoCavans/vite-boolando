b89fe54f4549ec75792283beaa882fde61911e9a
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-16 09:24:58 +0100

    Feat AppModal.vue - basic structure

---
+++ src/components/AppModal.vue
@@ -0,0 +1,61 @@
+<script>
+import { store } from "../store";
+
+export default {
+  data() {
+    return { store };
+  },
+
+  methods: {
+    cancelModal() {
+      store.modal.show = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="modal">
+    <div class="info-product">
+      <font-awesome-icon
+        class="toClick"
+        @click="cancelModal()"
+        icon="fa-solid fa-rectangle-xmark"
+      />
+      <h2>Prodotto</h2>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 139, 0.736);
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.info-product {
+  width: 500px;
+  background-color: white;
+  padding: 2rem;
+  border-radius: 20px;
+  color: black;
+  position: relative;
+}
+
+.toClick {
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  cursor: pointer;
+}
+</style>
